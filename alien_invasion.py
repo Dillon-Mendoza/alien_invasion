c7eeab6a5b37d17bbb406229a3a54613ee9435d1
Dillon-Mendoza <mudd.network@gmail.com>
2025-08-21 11:00:51 -0600

    Found an error in self.game_active. Placed self.stat.game_active by mistake causing game not to load.

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,15 +34,18 @@ class AlienInvasion:
         self.bg_color = (230, 230, 230)
 
         # Start Alien Invasion in an active state
-        self.stats.game_active = True
+        self.game_active = True
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+            
             print(len(self.bullets))
             self._update_screen()
             self.clock.tick(60)
